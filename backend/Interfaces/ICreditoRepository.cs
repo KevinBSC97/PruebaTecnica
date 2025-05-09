cc4e6c0df50b0fea6ccd0448d84416cb5996c3d0
Kevin <kevinsotomayor1@hotmail.com>
2025-05-09 18:15:29 -0500

    Se realizo la implementacion de la interfaz y repositorio de credito

---
+++ backend/Interfaces/ICreditoRepository.cs
@@ -0,0 +1,14 @@
+﻿using backend.Models;
+
+namespace backend.Interfaces
+{
+    public interface ICreditoRepository
+    {
+        Task<Credito> GetByIdAsync(int id);
+        Task<IEnumerable<Credito>> GetAllAsync();
+        Task<IEnumerable<Credito>> GetByUsuarioIdAsync(int usuarioId);
+        Task AddAsync(Credito credito);
+        Task UpdateAsync(Credito credito);
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ backend/Repositories/CreditoRepository.cs
@@ -0,0 +1,52 @@
+﻿using backend.Data;
+using backend.Interfaces;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repositories
+{
+    public class CreditoRepository : ICreditoRepository
+    {
+        private readonly AppDbContext _db;
+        public CreditoRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddAsync(Credito credito)
+        {
+            await _db.Creditos.AddAsync(credito);
+        }
+
+        public async Task<IEnumerable<Credito>> GetAllAsync()
+        {
+            return await _db.Creditos
+                .Include(c => c.Usuario)
+                .ToListAsync();
+        }
+
+        public async Task<Credito> GetByIdAsync(int id)
+        {
+            return await _db.Creditos
+                .Include(c => c.Usuario)
+                .FirstOrDefaultAsync(c => c.IdCredito == id);
+        }
+
+        public async Task<IEnumerable<Credito>> GetByUsuarioIdAsync(int usuarioId)
+        {
+            return await _db.Creditos
+                .Where(c => c.UsuarioId == usuarioId)
+                .ToListAsync();
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+        public async Task UpdateAsync(Credito credito)
+        {
+            _db.Creditos.Update(credito);
+        }
+    }
+}
