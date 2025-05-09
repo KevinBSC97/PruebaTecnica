6732a0227f1867fe12341c304842a43d36643ea7
Kevin <kevinsotomayor1@hotmail.com>
2025-05-09 17:55:30 -0500

    se realizo la implementacion de la interfaz y repositorio de usuario

---
+++ backend/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using backend.Models;
+
+namespace backend.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario?> GetEmailAsync(string email);
+        Task<Usuario?> GetByIdAsync(int id);
+        Task<IEnumerable<Usuario>> GetAllAsync();
+        Task AddAsync(Usuario usuario);
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ backend/Repositories/UsuarioRepository.cs
@@ -0,0 +1,41 @@
+﻿using backend.Data;
+using backend.Interfaces;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly AppDbContext _db;
+        public UsuarioRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddAsync(Usuario usuario)
+        {
+            await _db.Usuarios.AddAsync(usuario);
+        }
+
+        public async Task<IEnumerable<Usuario>> GetAllAsync()
+        {
+            return await _db.Usuarios.ToListAsync();
+        }
+
+        public async Task<Usuario?> GetByIdAsync(int id)
+        {
+            return await _db.Usuarios.FindAsync(id);
+        }
+
+        public async Task<Usuario?> GetEmailAsync(string email)
+        {
+            return await _db.Usuarios.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _db.SaveChangesAsync() > 0;
+        }
+    }
+}
