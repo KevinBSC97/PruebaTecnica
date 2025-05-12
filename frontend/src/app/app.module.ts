79460ba48e49c205b36a1d15ef5940ad06f82415
Kevin <kevinsotomayor1@hotmail.com>
2025-05-11 19:22:24 -0500

    Se agrega interceptor de autenticación para manejo de JWT en solicitudes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,13 @@ import { AppComponent } from './app.component';
     ToastModule,
     BrowserAnimationsModule
   ],
-  providers: [MessageService],
+  providers: [MessageService, [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ]],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
